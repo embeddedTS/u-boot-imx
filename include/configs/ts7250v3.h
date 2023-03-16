b0c85eb3a3024c1cf4a65f551cc4c041e1ec102a
Mark Featherston <mark@embeddedts.com>
2023-03-16 15:42:53 -0700

    configs: ts7250v3: Add netretry to default envirironment
    This will make it retry from the second ethernet port before giving up

--- include/configs/ts7250v3.h
+++ include/configs/ts7250v3.h
@@ -44,6 +44,7 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"autoload=no\0" \
 	"emmc_dev=0\0" \
+	"netretry=once\0" \
 	"clearenv=mmc dev 0 1; mmc erase 800 2000; mmc erase C00 2000;\0" \
 	"loadaddr=" __stringify(CONFIG_LOADADDR) "\0" \
 	"fdtaddr=" __stringify(FDT_ADDR_R) "\0" \
